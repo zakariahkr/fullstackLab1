9006ff92b1ac1ed2a440ef045a32b7fdcde42847
zakaria <zakaria.majkouma0013@stud.hkr.se>
2025-06-07 12:37:10 +0200

    Updated server.js

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.use('/api/dishes', dishesRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
     app.listen(PORT, () =>{
-        console.log('Server running on http://localhost:${PORT}');
+        console.log(`Server running on http://localhost:${PORT}`);
     });
 });
